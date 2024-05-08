b4f21235b845119ab3fcfc184d345470fe792898
Cob007 <godwill2beracah@outlook.com>
2024-05-08 06:04:05 -0400

    Project strutures and dependencies update added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './component/Header/Header'
+import Sample from './component/Sample/Sample'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Sample/>}  />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.scss'
+
+const Header = () => {
+
+    return (
+        <div className='header'>
+            <ul> 
+                <li>
+                    Home
+                </li>
+                <li>
+                    Project
+                </li>
+                <li>
+                    Resume
+                </li>
+                <li>
+                    About Me
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
