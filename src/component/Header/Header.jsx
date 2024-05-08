87a2b769a544112994a7e8ee12c1df31478e3912
Cob007 <godwill2beracah@outlook.com>
2024-05-08 16:22:26 -0400

    UI styling update added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,25 +1,31 @@
-import './Header.scss'
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  return (
+    <motion.div
+      className="header"
+      drag
+      dragConstraints={{
+        top: -50,
+        left: -50,
+        right: 50,
+        bottom: 50,
+      }}
+    >
+      <ul className="header__list">
+        <li className="header__item">
+          <NavLink>
+            <p> Home </p>
+            </NavLink>
+        </li>
+        <li className="header__item">Project</li>
+        <li className="header__item">Resume</li>
+        <li className="header__item">About Me</li>
+      </ul>
+    </motion.div>
+  );
+};
 
-    return (
-        <div className='header'>
-            <ul className='header__list'> 
-                <li className='header__item'>
-                    Home
-                </li>
-                <li className='header__item'>
-                    Project
-                </li>
-                <li className='header__item'>
-                    Resume
-                </li>
-                <li className='header__item'> 
-                    About Me
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
