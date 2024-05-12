1d13b11dbbd0cdd77a679174b7816a70287be7e3
Cob007 <godwill2beracah@outlook.com>
2024-05-11 22:44:43 -0400

    Project page update added

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,11 +1,47 @@
-import './Projects.scss'
+import { useRef } from "react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import "./Projects.scss";
+
+const data = [
+  {
+    id: 1,
+    title: "React Commerce",
+  },
+  {
+    id: 2,
+    title: "React Commerce",
+  },
+  {
+    id: 3,
+    title: "React Commerce",
+  },
+];
 
 const Projects = () => {
-    return (
-        <div className='portfolio'>
-            <p>Project</p>
-        </div>
-    )
-}
+  const ref = useRef();
 
-export default Projects
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="wrapper">
+        <h1>Project & Featured Works</h1>
+      </div>
+        {data.map((project) => (
+            <div>
+                <h3>{project.title}</h3>
+            </div>
+        ))}
+    </div>
+  );
+};
+
+export default Projects;
